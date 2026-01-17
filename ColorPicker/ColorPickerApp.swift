429870c5746b5732513ea825a97713a9add368fc
AdithyaSean <adithyasean@gmail.com>
2026-01-17 11:30:39 +0530

    Initial Commit

---
+++ ColorPicker/ColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPickerApp.swift
+//  ColorPicker
+//
+//  Created by Adithya Ekanayaka on 2026-01-17.
+//
+
+import SwiftUI
+
+@main
+struct ColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
